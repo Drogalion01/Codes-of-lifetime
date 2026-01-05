61bb9ecd04b8de5bbd73f90d914ea36fb7638a1b
Ashab Mahir <ashabmahir.200@gmail.com>
2026-01-05 22:27:10 +0600

    Football

--- Football.cpp
+++ Football.cpp
@@ -10,10 +10,47 @@ int main()
     {
         cout << n - (a + b) << endl;
         for(int i = 0; i < n - (a + b); i++) cout << "0:0" << endl;
-        for(int i = n - (a + b); i < n - b; i++) cout << "1:0" << endl;
-        for(int i = n - b; i < n; i++) cout << "0:1" << endl;
+        for(int i = 0; i < a; i++) cout << "1:0" << endl;
+        for(int i = 0; i < b; i++) cout << "0:1" << endl;
     }
 
-    else if(n == 1 && a==b)
-        cout << "1" << endl << a << ":" << a << endl;
+    else if(n == 1 && a==b) cout << "1" << endl << a << ":" << a << endl;
+    
+    else
+    {
+        cout << "0" << endl;
+
+        if (n == 1) cout << a << ":" << b << endl;
+        
+        else if (n == (a+b))
+        {
+            for(int i = 0; i < a; i++) cout << "1:0" << endl;
+            for(int i = 0; i < b; i++) cout << "0:1" << endl;
+        }
+        else
+        {
+            vector <int> arra(n, 0);
+            vector <int> arrb(n, 0);
+            arra[0] = a, arrb[0] = b;
+            for(int i = 1; i<n; i++)
+            {
+                if(arra[0]>0)
+                {
+                          arra[0]--;
+                          arra[i]++;
+                }
+                if(arrb[0]>0 && i > a)
+                {
+                          arrb[0]--;
+                          arrb[i]++;
+                }
+            }
+            if(arra[0] == arrb[0])
+            {
+                arra[0]--;
+                arra[n-1]++;
+            }
+            for (int i = 0; i<n; i++) cout << arra[i] << ":" << arrb[i] << endl;
+        }
+    }
 }
