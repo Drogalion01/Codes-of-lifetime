54103f3cee0052ad9a0f6f0fdf2762fade0dc5df
Ashab Mahir <ashabmahir.200@gmail.com>
2026-01-13 16:21:54 +0600

    Finally

--- Interesting_drink.cpp
+++ Interesting_drink.cpp
@@ -4,17 +4,27 @@ using namespace std;
 int main()
 {
     int n, m;
+ 
     cin >> n;
     vector <int> shop(n);
     for(auto& x : shop) cin >> x;
+    sort (shop.begin(), shop.end());
+    int mx = *max_element(shop.begin(), shop.end());
+    vector <int> v(100000, 0);
+    for(int i = 0; i < n; i++) v[shop[i]-1] ++;
+
+    vector <int> p(100001, 0);
+    for(int i = 1; i <= 100000; i++) p[i] = p[i-1] + v[i-1];
+
     cin >> m;
     vector<long long> coin(m);
-    vector <long long> d(m, 0);
     for(auto& x : coin) cin >> x;
-
+    vector<long long> d(m, 0);
     for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            if(coin[i] >= shop[j]) d[i]++;
+        if(coin[i] >= mx) d[i] = n;
+        else {
+            int temp = coin[i];
+            d[i] = p[min (temp, 100000)] - p[0];
         }
     }
     for(int i = 0; i < m; i++) cout << d[i] << endl;
