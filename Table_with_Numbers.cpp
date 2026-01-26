58e9fe8bd024d66f95b5b6b2b8f36003eb7cb594
Ashab Mahir <ashabmahir.200@gmail.com>
2026-01-26 23:21:24 +0600

    kobe

--- Table_with_Numbers.cpp
+++ Table_with_Numbers.cpp
@@ -9,9 +9,21 @@ int main(){
     while(test_case--){
         int n, h, l;
         cin >> n >> h >> l;
-        int a[h][l]={};
-        int temp = h*l;
-        cout << min((int)n/2, temp) << endl;
+        vector <int> a(n);
+        for(auto& x : a) cin >> x;
+        vector<vector<int>> t(h, vector<int>(l, 0));
+        for(int i = 0; i < n-1; i++){
+            if(a[i] <= h && a[i+1] <= l){
+                t[a[i]-1][a[i+1]-1] = 1;
+            }
+        }
+        int s = 0;
+        for(int i = 0; i < h; i++){
+            for(int j = 0; j < l; j++){
+                s += t[i][j];
+            }
+        }
+        cout << s << endl;
     }
     return 0;
 }
