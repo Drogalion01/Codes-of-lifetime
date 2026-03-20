cd954c358cb5e4d23dcbde7c70da8a81a12954d4
Ashab Mahir <ashabmahir.200@gmail.com>
2026-03-20 22:28:42 +0600

    Eid Mubarak

--- B_Cyclists.cpp
+++ B_Cyclists.cpp
@@ -5,7 +5,21 @@ void solve(){
     cin >> n >> k >> p >> m;
     vector<int> a(n);
     for(auto& x : a) cin >> x;
-    
+    vector<long long> tmp;
+
+        long long turn = a[0]*p;
+        for(int i = 1; i < p; i++){
+            turn = min(turn, (long long)(a[i]*(p-i)));
+        }
+        int rem = m-turn;
+        if(rem < 0) cout << "0" << endl;
+        else {
+            int cycle = 0;
+            for(int i = 0; i < k; i++){
+                cycle = max(cycle, rem / (a[i]*(n-i)));
+            }
+            cout << cycle+1 << endl;
+        }
 
 }
 int main(){
---
+++ C_Vasilije_in_Cacak.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    long long n, k, x;
+    cin >> n >> k >> x;
+    // int t = (k*(2*n - k + 1))/2;
+    // cout << t << endl;
+    if(x >= ((k*(k+1))/2) && x <= (k*(2*n - k + 1))/2) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int test_case;
+    cin >> test_case;
+    while(test_case--){
+        solve();
+    }
+    return 0;
+}
