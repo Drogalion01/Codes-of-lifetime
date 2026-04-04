41a930189938fff6103855251b2e970deb95a25f
Ashab Mahir <ashabmahir.200@gmail.com>
2026-04-04 20:04:02 +0600

    Brain wreck

--- B_Seats.cpp
+++ B_Seats.cpp
@@ -5,12 +5,56 @@ void solve(){
     cin >> n;
     string s;
     cin >> s;
+    
+    int one = count(s.begin(), s.end(), '1');
+    if(one == n) {
+        cout << one << endl;
+        return;
+    }
 
-    int z = count(s.begin(), s.end(), '0'), o = count(s.begin(), s.end(), '1');
+    vector<pair<int,int>>zero;
+    
+    bool z = false;
+    int cnt = 0;
+    for(int i = 0; i < n; i++){
+        if(s[i]=='0') {
+            cnt++;
+            if(i==0) z = true;
+        }
+        else {
+            if(z) zero.push_back({cnt, 1});
+            else zero.push_back({cnt, 0});
+            cnt = 0;
+            z = false;
+        }
+    }
+    zero.push_back({cnt, 1});
+    // for(int i = 0; i < zero.size(); i++){
+    //     cout << zero[i].first << " ";
+    // }
+    // cout << endl;
 
-    if(o > (n + 2)/3) cout << o << endl;
-    else if (n > 2 && (s[0] == '1' || s[2] == '1') && (s[n-1] == '1' || s[n-3] == '1' || n==3) && n%2) cout << (n + 2)/3 + 1 << endl;
-    else cout << (n + 2)/3 << endl;
+    if(n==count(s.begin(), s.end(), '0')) {
+        if(n%3==0) {
+            cout << n/3 << endl;
+            return;
+        }
+        else {
+            cout << n/3+1 << endl;
+            return;
+        }
+    }
+
+    for(int i = 0; i < zero.size(); i++){
+        if(zero[i].second == 1 && (zero[i].first+3)/3 == (zero[i].first+1)/3) {
+            one += (int)(zero[i].first/3) + 1;
+        }
+        else {
+            one += (int)(zero[i].first/3);
+        }
+    }
+
+    cout << one << endl;
 }
 int main(){
     ios::sync_with_stdio(false);
