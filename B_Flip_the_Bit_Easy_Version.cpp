545eeeebce0319e8f2f3114482a79e6d6168738d
Ashab Mahir <ashabmahir.200@gmail.com>
2026-04-16 10:55:10 +0600

    Ki hoise amr

--- B_Flip_the_Bit_Easy_Version.cpp
+++ B_Flip_the_Bit_Easy_Version.cpp
@@ -3,7 +3,22 @@ using namespace std;
 void solve(){
     int n, k;
     cin >> n >> k;
+    vector <int> a(n);
     for(auto& x : a) cin >> x;
+    int ik;
+    cin >> ik;
+    int cl = 0, cr = 0, t = a[0];
+    for(int i = 1; i < n; i++){
+            if(t!=a[i]) {
+                if(i < ik) cl++;
+                else cr++;
+            } 
+            t = a[i];
+    }
+    // cout << cl << " " << cr << endl;
+    int out = max(cl, cr);
+    if(out%2) out++;
+    cout << out << endl;
 }
 int main(){
     ios::sync_with_stdio(false);
