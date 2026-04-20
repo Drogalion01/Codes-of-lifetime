67da833174549f5a38f4cc2a0510112c56cec8cb
Ashab Mahir <ashabmahir.200@gmail.com>
2026-04-20 21:07:59 +0600

    Hoise

---
+++ A_Grid_L.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+void solve(){
+    int a, b;
+    cin >> a >> b;
+    int tmp = a + 2 * b;
+
+    for(int i = 1; i <= 10e6; i++){
+        int den = 2 * i + 1;
+        int num = tmp - i;
+
+        if(num <= 0) break;
+        if(num % den != 0) continue;
+
+        int j = num / den;  
+        if(j < 1) continue;
+
+        if(abs(i - j) <= a){
+            cout << i << " " << j << endl;
+            return;
+        }
+    }
+    cout << "-1" << endl;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int test_case;
+    cin >> test_case;
+    while(test_case--){
+        solve();
+    }
+    return 0;
+}
