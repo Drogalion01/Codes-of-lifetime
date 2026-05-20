6a2968cc482e24ed043a9ad326768c92a089feca
Ashab Mahir <ashabmahir.200@gmail.com>
2026-05-20 22:44:59 +0600

    Fasik

--- B_Alternating_String.cpp
+++ B_Alternating_String.cpp
@@ -7,19 +7,10 @@ void solve(){
     int c = 0;
     vector <int> tmp;
     for(int i = 0; i + 1 < s.size(); i++){
-        if(s[i] == s[i+1]) {
-            c++;
-            tmp.push_back(i+1);
-        }
+        if(s[i] == s[i+1]) c++;
     }
-    if (c > 2) {
-        
-        cout << "NO" << endl;
-    }
-    else {
-        cout << "YES" << endl;
-    }
-    
+    if (c > 2) cout << "NO" << endl;
+    else cout << "YES" << endl;    
 }
 int main(){
     ios::sync_with_stdio(false);
